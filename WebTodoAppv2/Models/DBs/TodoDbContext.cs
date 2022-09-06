ba18d78be2223fe2ffd31ccc3a3ded8df70c26ff
Akn <sibata486@gmail.com>
2022-09-06 18:52:54 +0900

    add / TodoDbContext クラスを追加

---
+++ WebTodoAppv2/Models/DBs/TodoDbContext.cs
@@ -0,0 +1,27 @@
+﻿namespace WebTodoAppv2.Models.DBs
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using Npgsql;
+
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+
+            builder.Port = 5433;
+            builder.Username = "postgres";
+            builder.Password = "password";
+            builder.Host = "localhost";
+            builder.Database = "testdb";
+
+            optionsBuilder.UseNpgsql(builder.ToString());
+        }
+    }
+}
