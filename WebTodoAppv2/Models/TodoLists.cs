2c5c2465536c69239cef3f7ec905bcafbea383b1
Akn <sibata486@gmail.com>
2022-09-08 23:19:44 +0900

    fix / TodoLists.Todos を変更通知を出すプロパティに変更

--- WebTodoAppv2/Models/TodoLists.cs
+++ WebTodoAppv2/Models/TodoLists.cs
@@ -5,9 +5,10 @@
 
     public class TodoLists : BindableBase
     {
+        private ObservableCollection<Todo> todos;
         private Todo selectionItem;
 
-        public ObservableCollection<Todo> Todos { get; set; }
+        public ObservableCollection<Todo> Todos { get => todos; set => SetProperty(ref todos, value); }
 
         public Todo SelectionItem { get => selectionItem; set => SetProperty(ref selectionItem, value); }
     }
