9aea29a8433ee8a6ffda176e7f99561fd06f3f43
Akn <sibata486@gmail.com>
2022-09-09 00:36:01 +0900

    add / 画面右側に Todo の操作ログを表示する ListView を追加

--- WebTodoAppv2/Models/TodoLists.cs
+++ WebTodoAppv2/Models/TodoLists.cs
@@ -6,9 +6,12 @@
     public class TodoLists : BindableBase
     {
         private ObservableCollection<Todo> todos;
+        private ObservableCollection<Operation> operations;
         private Todo selectionItem;
 
         public ObservableCollection<Todo> Todos { get => todos; set => SetProperty(ref todos, value); }
+
+        public ObservableCollection<Operation> Operations { get => operations; set => SetProperty(ref operations, value); }
 
         public Todo SelectionItem { get => selectionItem; set => SetProperty(ref selectionItem, value); }
     }
