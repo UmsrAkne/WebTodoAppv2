052f456c3041cf5ea6abdaa24c43b9a2241a246a
Akn <sibata486@gmail.com>
2022-09-10 23:57:48 +0900

    add / Todo 詳細を表示するウィンドウのクラスとビューモデルを追加

---
+++ WebTodoAppv2/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace WebTodoAppv2.ViewModels
+{
+    using System;
+    using Prism.Services.Dialogs;
+    using WebTodoAppv2.Models;
+    using WebTodoAppv2.Models.DBs;
+
+    public class DetailPageViewModel : IDialogAware
+    {
+        private TodoDbContext todoDbContext;
+        private TodoLists todoLists;
+
+        public DetailPageViewModel(TodoDbContext todoDbContext, TodoLists todoLists)
+        {
+            this.todoDbContext = todoDbContext;
+            this.todoLists = todoLists;
+        }
+
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => "Detail Page";
+
+        public bool CanCloseDialog() => true;
+
+        public void OnDialogClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
---
+++ WebTodoAppv2/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace WebTodoAppv2.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// DetailPage.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
