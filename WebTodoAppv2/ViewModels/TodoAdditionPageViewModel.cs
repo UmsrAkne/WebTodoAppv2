014f54f074824f62020a2e0ae263ad411b5be52c
Akn <sibata486@gmail.com>
2022-09-15 00:16:50 +0900

    add / Todo追加ページとそのビューモデルを追加

---
+++ WebTodoAppv2/ViewModels/TodoAdditionPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace WebTodoAppv2.ViewModels
+{
+    using System;
+    using Prism.Services.Dialogs;
+    using WebTodoAppv2.Models;
+    using WebTodoAppv2.Models.DBs;
+
+    public class TodoAdditionPageViewModel : IDialogAware
+    {
+        private TodoDbContext todoDbContext;
+        private TodoLists todoLists;
+
+        public TodoAdditionPageViewModel(TodoDbContext todoDbContext, TodoLists todoLists)
+        {
+            this.todoDbContext = todoDbContext;
+            this.todoLists = todoLists;
+        }
+
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => "Create new Todo";
+
+        public bool CanCloseDialog() => true;
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
