c9204c4e77aa2e813500af385c7bca3d5a71bd5d
Akn <sibata486@gmail.com>
2022-09-17 01:47:15 +0900

    add / Todo のグループを表すクラスを追加

---
+++ WebTodoAppv2/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿namespace WebTodoAppv2.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Prism.Mvvm;
+
+    public class Group : BindableBase
+    {
+        private string name;
+        private bool editMode;
+
+        public int Id { get; set; }
+
+        public string Name { get => name; set => SetProperty(ref name, value); }
+
+        [NotMapped]
+        public bool EditMode { get => editMode; set => SetProperty(ref editMode, value); }
+    }
+}
