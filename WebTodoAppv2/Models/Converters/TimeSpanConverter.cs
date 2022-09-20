6d6d6a225152ecbac893a77de182ffd26e5cf79b
Akn <sibata486@gmail.com>
2022-09-20 22:39:42 +0900

    add / 総作業時間の表示のために TimeSpanConverter を実装

---
+++ WebTodoAppv2/Models/Converters/TimeSpanConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace WebTodoAppv2.Models.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class TimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return TimeSpan.FromSeconds(Math.Floor(((TimeSpan)value).TotalSeconds));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
